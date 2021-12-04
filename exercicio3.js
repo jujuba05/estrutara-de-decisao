0fbcdddae5f6c36f2fa0628e16f34deb8a3b23a7
jujuba05 <95108515+jujuba05@users.noreply.github.com>
2021-12-04 16:40:12 -0300

    Create exercicio3.js

---
+++ exercicio3.js
@@ -0,0 +1,11 @@
+var num1 = parseInt(prompt('digite o primeiro numero'));
+var maior = num1;
+var num2 = parseInt(prompt('digite o segundo numero'));
+se(num2 > maior ){
+    maior=num2;
+}
+var num3 = parseInt(prompt('digite o terceiro numero'));
+se (num3 > maior ) {
+    maior= num3
+}
+alerta('o maior numero digitado foi: ' + maior);
